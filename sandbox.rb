1a818ce50a996bf40a5711d651735518b93cce99
Chante Earthwell <chante.earthwell@buildertrend.com>
2019-07-19 12:58:04 -0500

    continued tinkering with split()

--- sandbox.rb
+++ sandbox.rb
@@ -1,8 +1,15 @@
 a = ["10","","","","20","30","","40","",""]
+b = []
 
 #CE try this: https://stackoverflow.com/questions/18924068/how-do-i-iterate-through-a-file-in-ruby-while-using-peek-and-not-lose-half-of
-a.each_with_index do |x, i|
-	if i == ""
-		puts x 
+a.each_cons(2) do |x|
+	if x == ["", ""]
+		b.push(0)
+	else
+		b.push(1)
 	end
 end
+
+print b
+
+#I could track which index and use that to manually navigate through actual array.push??
